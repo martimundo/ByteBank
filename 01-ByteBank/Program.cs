05d0a37dc3a60895823bbcb967d375b99e0311ea
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2022-02-03 21:48:43 -0300

    Add project files.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Marcos Roberto Artimundo";
+            int numAgencia = 863;
+            int numConta = 863146;
+            int saldo = 100;
+
+        }
+    }
+}
